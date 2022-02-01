90b114eaaf95ddea4c711a1f421e1a46dc22ff0a
Jasper Chan <jasperchan515@gmail.com>
2022-02-01 03:49:27 -0800

    release: first release

---
+++ examples/sample_py.py
@@ -0,0 +1,25 @@
+from math import pi
+
+params = [
+    {
+        "type": "revolute",
+        "d": 2,
+        "theta": pi/2,
+        "r": 0,
+        "alpha": pi/2
+    },
+    {
+        "type": "revolute",
+        "d": 2,
+        "theta": pi/2,
+        "r": 0,
+        "alpha": -pi/2
+    },
+    {
+        "type": "revolute",
+        "d": 2,
+        "theta": pi/2,
+        "r": 2,
+        "alpha": 0
+    },
+]
---
+++ tests/test_dh2vrml.py
@@ -0,0 +1,5 @@
+from dh2vrml import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'

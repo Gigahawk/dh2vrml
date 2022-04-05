68867d87f02b7a8aaf8e683a6e7e2f5c2daf2aa8
Jasper Chan <jasperchan515@gmail.com>
2022-04-05 02:40:49 -0700

    rev: couple revision to pyproject

--- dh2vrml/__init__.py
+++ dh2vrml/__init__.py
@@ -1,1 +1,7 @@
-__version__ = '0.1.6'
+from importlib import metadata
+import toml
+
+try:
+    __version__ = metadata.version(__package__)
+except metadata.PackageNotFoundError:
+    __version__ = toml.load("pyproject.toml")["tool"]["poetry"]["version"] + "dev"
--- tests/test_dh2vrml.py
+++
@@ -1,5 +0,0 @@
-from dh2vrml import __version__
-
-
-def test_version():
-    assert __version__ == '0.1.6'
